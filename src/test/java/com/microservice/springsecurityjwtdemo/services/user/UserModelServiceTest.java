263762af5fdd31defdf2bb5c4a803ccf498b8440
RonyLiboni <ronald.liboni@acad.pucrs.br>
2022-09-13 15:12:22 -0300

    TEST UserModelService adds test for token creation for new password

--- src/test/java/com/microservice/springsecurityjwtdemo/services/user/UserModelServiceTest.java
+++ src/test/java/com/microservice/springsecurityjwtdemo/services/user/UserModelServiceTest.java
@@ -1,14 +1,37 @@
 package com.microservice.springsecurityjwtdemo.services.user;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import com.microservice.springsecurityjwtdemo.entities.user.UserModel;
+import com.microservice.springsecurityjwtdemo.repositories.UserRepository;
+import com.microservice.springsecurityjwtdemo.services.emails.EmailSenderService;
 
+@ExtendWith(SpringExtension.class)
 class UserModelServiceTest {
-
+	
+	@InjectMocks
+	private UserModelService userModelService;
+	@Mock
+	private UserRepository userRepository;
+	@Mock
+	private PasswordEncoder passwordEncoder;
+	@Mock
+	private EmailSenderService emailSenderService;
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void sendEmailWithTokenToCreateANewPassword_ShouldCreateAToken() {
+		Mockito.when(userRepository.findByUsername(ArgumentMatchers.anyString())).thenReturn(Optional.of(new UserModel()));
+		String username = "ronald.liboni@acad.pucrs.br";
+		userModelService.sendEmailWithTokenToCreateANewPassword(username);
+		Mockito.verify(userRepository).findByUsername(username);
+		Mockito.verify(emailSenderService).sendEmail(ArgumentMatchers.anyString(), ArgumentMatchers.anyString(), ArgumentMatchers.anyString());
 	}
 
 }
