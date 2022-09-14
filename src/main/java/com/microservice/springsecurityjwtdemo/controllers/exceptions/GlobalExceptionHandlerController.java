3dd0aca9089c23d32738acc0f819f15a3aaac693
RonyLiboni <ronald.liboni@acad.pucrs.br>
2022-09-14 14:10:05 -0300

    REFACTOR (GlobalExceptionHandlerController) uses stream to get a list of field errors

--- src/main/java/com/microservice/springsecurityjwtdemo/controllers/exceptions/GlobalExceptionHandlerController.java
+++ src/main/java/com/microservice/springsecurityjwtdemo/controllers/exceptions/GlobalExceptionHandlerController.java
@@ -1,16 +1,13 @@
 package com.microservice.springsecurityjwtdemo.controllers.exceptions;
 
-import java.util.ArrayList;
 import java.util.List;
-
+import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import com.microservice.springsecurityjwtdemo.entities.validation.field_errors.FieldErrorsDto;
-
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 
@@ -21,13 +18,10 @@ public class GlobalExceptionHandlerController {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	@ApiResponse(responseCode= "400", description = "There was an error with the form you sent.")
 	public ResponseEntity<List<FieldErrorsDto>> formFieldsHasErrors(MethodArgumentNotValidException exception) {
-		List<FieldErrorsDto> fieldErrorsdto = new ArrayList<>();
-		
-		exception.getBindingResult().getFieldErrors().forEach(error ->{
-			fieldErrorsdto.add(new FieldErrorsDto(error));
-		});;
-
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(fieldErrorsdto);
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+				.body(exception.getBindingResult().getFieldErrors().stream()
+													 .map(FieldErrorsDto::new)
+													 .collect(Collectors.toList()));
 	}
 	
 	@ExceptionHandler(RuntimeException.class)
