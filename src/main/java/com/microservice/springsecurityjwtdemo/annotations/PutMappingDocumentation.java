7381266d82f0ad63a7eceedb7e1611ce4c4628e3
RonyLiboni <ronald.liboni@acad.pucrs.br>
2022-10-14 11:52:30 -0300

    STYLE change globalException package from controller

--- src/main/java/com/microservice/springsecurityjwtdemo/annotations/PutMappingDocumentation.java
+++ src/main/java/com/microservice/springsecurityjwtdemo/annotations/PutMappingDocumentation.java
@@ -9,7 +9,6 @@ import java.lang.annotation.Target;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @Target({METHOD})
 @Retention(RetentionPolicy.RUNTIME)
