ed6396b6fc974511f44e003eed2824c1e22c6174
RonyLiboni <ronald.liboni@acad.pucrs.br>
2022-10-31 14:57:12 -0300

    FEAT adds the app in the server discovery

--- src/main/java/com/microservice/springsecurityjwtdemo/SpringSecurityJwtDemoApplication.java
+++ src/main/java/com/microservice/springsecurityjwtdemo/SpringSecurityJwtDemoApplication.java
@@ -2,8 +2,10 @@ package com.microservice.springsecurityjwtdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringSecurityJwtDemoApplication {
 
 	public static void main(String[] args) {
