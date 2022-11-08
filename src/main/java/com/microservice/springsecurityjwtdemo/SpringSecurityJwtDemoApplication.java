add20bb35b17f43f0d20e9918b95d34189164202
RonyLiboni <ronald.liboni@acad.pucrs.br>
2022-11-08 14:19:18 -0300

    DOC set the gateway server as default to swaggger

--- src/main/java/com/microservice/springsecurityjwtdemo/SpringSecurityJwtDemoApplication.java
+++ src/main/java/com/microservice/springsecurityjwtdemo/SpringSecurityJwtDemoApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @SpringBootApplication
 @EnableEurekaClient
+@OpenAPIDefinition(servers = { @Server(url = "http://localhost:8080/security-ms", description = "Gateway URL") })
 public class SpringSecurityJwtDemoApplication {
 
 	public static void main(String[] args) {
